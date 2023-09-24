95ada1b42a7047b135589b5ca4b2c5ea949120b6
Sayan Banerjee <sayankb.001@gmail.com>
2023-09-24 15:07:11 +0530

    06-call-apply-bind all function are updated

--- 06-Call-Apply-Bind/main.js
+++ 06-Call-Apply-Bind/main.js
@@ -1,66 +1,131 @@
 // Creating own call, apply, bind ==========================
 
-// call ==========
+// call version = 1 ==========
+// Function.prototype.myCall = function (context, ...args) {
+//   if (typeof this !== "function") {
+//     throw new TypeError("myCall can only be used on functions");
+//   }
+//   const uniqueId = Symbol();
+//   context[uniqueId] = this;
+//   const result = context[uniqueId](...args);
+//   delete context[uniqueId];
+//   return result;
+// };
+
+// call version = 2 ==========
 Function.prototype.myCall = function (context, ...args) {
-  if (typeof this !== "function") {
-    throw new TypeError("myCall can only be used on functions");
+  let returnValue
+  if (context == null || context == undefined) {
+    return this()
   }
-  const uniqueId = Symbol();
-  context[uniqueId] = this;
-  const result = context[uniqueId](...args);
-  delete context[uniqueId];
-  return result;
-};
+  if (typeof context == 'object') {
+    const uniqueId = Symbol()
+    context[uniqueId] = this
+    returnValue = context[uniqueId](...args)
+    delete context[uniqueId]
+    return returnValue
+  }
+  context = new Object(context)
+  const uniqueId = Symbol()
+  context[uniqueId] = this
+  returnValue = context[uniqueId](...args)
+  delete context[uniqueId]
+  return returnValue
+}
 
 function user1(name) {
-  this.name = name;
+  this.name = name
   function showUser1(a, b) {
-    console.log(this.name, a, b, " --> call");
+    console.log(this.name, a, b, ' --> call')
   }
-  showUser1.myCall(this, "arg1", "arg2"); // Using my own call method
+  showUser1.myCall(this, 'arg1', 'arg2') // Using my own call method
 }
-const userObj1 = new user1("Sayan");
+const userObj1 = new user1('Sayan')
 
-// apply ==========
-Function.prototype.myApply = function (context, argsArray) {
-  if (typeof this !== "function") {
-    throw new TypeError("myCall can only be used on functions");
+// apply version = 1 ==========
+// Function.prototype.myApply = function (context, argsArray) {
+//   if (typeof this !== "function") {
+//     throw new TypeError("myCall can only be used on functions");
+//   }
+//   const uniqueId = Symbol();
+//   context[uniqueId] = this;
+//   const result = context[uniqueId](argsArray);
+//   delete context[uniqueId];
+//   return result;
+// };
+
+// apply version = 2 ==========
+Function.prototype.myApply = function (context, args) {
+  let returnValue
+  if (context == null || context == undefined) {
+    return this()
   }
-  const uniqueId = Symbol();
-  context[uniqueId] = this;
-  const result = context[uniqueId](argsArray);
-  delete context[uniqueId];
-  return result;
-};
+  if (typeof context == 'object') {
+    const uniqueId = Symbol()
+    context[uniqueId] = this
+    returnValue = context[uniqueId](args)
+    delete context[uniqueId]
+    return returnValue
+  }
+  context = new Object(context)
+  const uniqueId = Symbol()
+  context[uniqueId] = this
+  returnValue = context[uniqueId](args)
+  delete context[uniqueId]
+  return returnValue
+}
 
 function user2(name) {
-  this.name = name;
+  this.name = name
   function showUser2([a, b]) {
-    console.log(this.name, a, b, " --> apply");
+    console.log(this.name, a, b, ' --> apply')
   }
-  showUser2.myCall(this, ["arg1", "arg2"]); // Using my own call method
+  showUser2.myCall(this, ['arg1', 'arg2']) // Using my own call method
 }
-const userObj2 = new user2("Sayan");
+const userObj2 = new user2('Sayan')
 
-// Bind =====================
+// Bind version = 1 =====================
+// Function.prototype.myBind = function (context, ...args) {
+//   if (typeof this !== 'function') {
+//     throw new TypeError('myCall can only be used on functions')
+//   }
+//   const originalFunction = this
+//   return function () {
+//     const uniqueId = Symbol()
+//     context[uniqueId] = originalFunction
+//     context[uniqueId](...args)
+//     delete context[uniqueId]
+//   }
+// }
+
+// Bind version = 2 =====================
 Function.prototype.myBind = function (context, ...args) {
-  if (typeof this !== "function") {
-    throw new TypeError("myCall can only be used on functions");
+  return (...newArgs) => {
+    let returnValue
+    if (context == null || context == undefined) {
+      return this()
+    }
+    if (typeof context == 'object') {
+      const uniqueId = Symbol()
+      context[uniqueId] = this
+      returnValue = context[uniqueId](...[...args, ...newArgs])
+      delete context[uniqueId]
+      return returnValue
+    }
+    context = new Object(context)
+    const uniqueId = Symbol()
+    context[uniqueId] = this
+    returnValue = context[uniqueId](...[...args, ...newArgs])
+    delete context[uniqueId]
+    return returnValue
   }
-  const originalFunction = this;
-  return function () {
-    const uniqueId = Symbol();
-    context[uniqueId] = originalFunction;
-    context[uniqueId](...args);
-    delete context[uniqueId];
-  };
-};
+}
 
 const userObj3 = {
-  name: "Sayan",
-};
-function showUser3(a, b) {
-  console.log(this.name, a, b, " --> bind");
+  name: 'Sayan',
 }
-const showUser3Bound = showUser3.myBind(userObj2, "arg1", "arg2");
-showUser3Bound();
+function showUser3(a, b) {
+  console.log(this.name, a, b, ' --> bind')
+}
+const showUser3Bound = showUser3.myBind(userObj2, 'arg1', 'arg2')
+showUser3Bound()
